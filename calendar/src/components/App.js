a25b15b741c91249781847bbb9424e8ded202d61
Patria <patria2826@gmail.com>
2019-09-12 17:56:57 +0800

    calendar

--- calendar/src/components/App.js
+++ calendar/src/components/App.js
@@ -1,59 +1,82 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './css/app.css';
+import React from 'react';
+import Calendar from './Calendar';
 
-class App extends Component {
-    state = {
-        completeData: [],
-        monthsShowed: [],
-        selectedMonth: null
-    }
-    weekdays = [
-        '星期日',
-        '星期一',
-        '星期二',
-        '星期三',
-        '星期四',
-        '星期五',
-        '星期六',
-    ]
-    allMonths = [];
-    getData = async () => {
-        const response = await axios.get('./data1.json', {
-            headers: {
-                'Accept': 'application/json'
-            }
-        });
-        this.setState({ completeData: response.data });
+const App = () => {
+    // state = {
+    //     completeData: [],
+    //     monthsShowed: [],
+    //     selectedMonth: null
+    // }
+    // weekdays = [
+    //     '星期日',
+    //     '星期一',
+    //     '星期二',
+    //     '星期三',
+    //     '星期四',
+    //     '星期五',
+    //     '星期六',
+    // ]
+    // allMonths = [];
+    // getData = async () => {
+    //     const response = await axios.get('./data1.json', {
+    //         headers: {
+    //             'Accept': 'application/json'
+    //         }
+    //     });
+    //     this.setState({ completeData: response.data });
 
-        // get year and month from data
-        this.allMonths = Array.from(new Set(response.data.map((data) => { return data['date'].substring(0, 7) }))).sort();
-        this.setState({ monthsShowed: this.allMonths.slice(0, 3) }, () => { this.setState({ selectedMonth: this.state.monthsShowed[0] }) }); //not sure
+    //     // get year and month from data
+    //     this.allMonths = Array.from(new Set(response.data.map((data) => { return data['date'].substring(0, 7) }))).sort();
+    //     this.setState({ monthsShowed: this.allMonths.slice(0, 3) }, () => { this.setState({ selectedMonth: this.state.monthsShowed[0] }) }); //not sure
 
-    }
-    getDays = () => {
-        
-    }
-    componentDidMount() {
-        this.getData();
-    }
-    render() {
-        return (
-            <div className="calender display-flex">
-                <div className="calendar-top display-flex">
-                    <a className="arrow" href="#"></a>
-                    <ul className="month-list display-flex">
-                        {this.state.monthsShowed.map((ele, i) => { return <li key={i}><a href="#">{ele.replace('/', ' ')}月</a></li> })}
-                    </ul>
-                    <a className="arrow arrow-right" href="#"></a>
-                </div>
-                <ul className="weekdays display-flex">
-                    {this.weekdays.map((ele, i) => { return <li key={i}>{ele}</li> })}
+    // }
+    // getDays = () => {
+
+    // }
+    // componentDidMount() {
+    //     this.getData();
+    // }
+    // render() {
+    return (
+        <div>
+            {/* <div className="calendar-top display-flex">
+                <a className="arrow" href="#"></a>
+                <ul className="month-list display-flex">
+                    {this.state.monthsShowed.map((ele, i) => { return <li key={i}><a href="#">{ele.replace('/', ' ')}月</a></li> })}
                 </ul>
-                <div className="month-days">
-                </div>
+                <a className="arrow arrow-right" href="#"></a>
             </div>
-        )
-    }
+            <ul className="weekdays display-flex">
+                {this.weekdays.map((ele, i) => { return <li key={i}>{ele}</li> })}
+            </ul>
+            <div className="month-days">
+            </div> */}
+            <Calendar
+                dataSource={'./data1.json'}
+                initYearMonth={'201705'}
+                dataKeySetting={{
+                    'guaranteed': 'guaranteed',
+                    // 狀態
+                    'status': 'status',
+                    // 可賣團位
+                    'available': 'availableVancancy',
+                    // 團位
+                    'total': 'totalVacnacy',
+                    // 價格
+                    'price': 'price'
+                }}
+                onClickPrev={function ($btn, data, module) {
+                    console.log($btn, data, module);
+                }}
+                onClickNext={function ($btn, data, module) {
+                    console.log($btn, data, module);
+                }}
+                onClickDate={function ($date, data) {
+                    console.log($date, data);
+                }}
+            />
+        </div>
+    )
+    // }
 }
 export default App;
---
+++ calendar/src/components/Calendar.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import './css/Calendar.css';
+
+const Calendar = (props) => {
+    // set state and get props
+    const [completeData, setCompleteData] = useState(0);
+    const [monthsShowed, setMonthsShowed] = useState(0);
+    const { dataSource, initYearMonth, dataKeySetting, onClickPrev, onClickNext, onClickDate } = props;
+
+    // set variants
+    const weekdays = [
+        '星期日',
+        '星期一',
+        '星期二',
+        '星期三',
+        '星期四',
+        '星期五',
+        '星期六',
+    ];
+    let allMonths = useRef();
+
+    const getData = async () => {
+        return await axios.get(dataSource, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+    }
+    const response = getData();
+
+
+    useEffect(() => {
+        if (response.data) { setCompleteData(response.data) }
+    }, [response.data]);
+    useEffect(() => {
+        if (completeData) { allMonths.current = Array.from(new Set(completeData.map((data) => { return data['date'].substring(0, 7) }))).sort(); }
+    })
+
+    return (
+        <div className="calender display-flex">
+            {/* <div className="calendar-top display-flex">
+                <a className="arrow" href="#"></a>
+                <ul className="month-list display-flex">
+                    {monthsShowed.map((ele, i) => { return <li key={i}><a href="#">{ele.replace('/', ' ')}月</a></li> })}
+                </ul>
+                <a className="arrow arrow-right" href="#"></a>
+            </div>
+            <ul className="weekdays display-flex">
+                {weekdays.map((ele, i) => { return <li key={i}>{ele}</li> })}
+            </ul>
+            <div className="month-days">
+            </div> */}
+        </div>
+    )
+}
+export default Calendar;
