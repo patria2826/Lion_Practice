53bcfca09cf6cc7d21afd20ccbb68b4220ed6e8b
Trina Lu <trina.lu@linecorp.com>
2020-06-18 15:02:01 +0900

    chore: apply prettier to index.js

--- banner_react/src/index.js
+++ banner_react/src/index.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import * as serviceWorker from "./serviceWorker";
 
 let app;
-ReactDOM.render(<App ref={inst => {
-    app = inst;
-}} />, document.getElementById('root'));
+ReactDOM.render(
+  <App
+    ref={(inst) => {
+      app = inst;
+    }}
+  />,
+  document.getElementById("root")
+);
 
 window.banner = function (instruction) {
-    if (instruction === 'open' || instruction === 'close' || instruction === 'toggle') {
-        app.banner(instruction)
-    } else {
-        console.error();
-    }
-}
+  if (
+    instruction === "open" ||
+    instruction === "close" ||
+    instruction === "toggle"
+  ) {
+    app.banner(instruction);
+  } else {
+    console.error();
+  }
+};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
